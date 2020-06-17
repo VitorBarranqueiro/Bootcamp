7c97a23bd7542af59a6f37181c49e0f35f01fe77
Vitor Santos <vtrbarranqueiro@hotmail.com>
2020-06-17 20:35:17 -0300

    atualizado com aulas de vetores e objetos

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const aluno01= 'Vitor'
-const notaAluno01= 9.8
+const aluno01 = {
+    nome: "Vitor",
+    nota: 9.8
+}
 
-const aluno02= 'Mayk'
-const notaAluno02= 10
+const aluno02 = {
+    nome: "Mayk",
+    nota: 10}
 
-const aluno03= 'Diego'
-const notaAluno03= 2
+const aluno03 = {
+    nome: "Diego",
+    nota: 2
+}
+
+
  
-const media= (notaAluno01 + notaAluno02 + notaAluno03) / 3
+const media= (aluno01.nota + aluno02.nota + aluno03.nota) / 3
 
 if (media >= 5) {
     console.log(`A media foi de ${media}. Parabens`)
