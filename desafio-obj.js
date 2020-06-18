8a25a480357cf1048dfb3226b9700c509656892d
Vitor Santos <vtrbarranqueiro@hotmail.com>
2020-06-17 21:04:53 -0300

    desafio com objetos

---
+++ desafio-obj.js
@@ -0,0 +1,13 @@
+const empresa = {
+    nome:"Rocketseat",
+    cor:"Roxo",
+    foco:"Programação",
+    endereço: {
+        Rua: "Rua Guilherme Gembala",
+        numero: 260
+    }
+}
+
+console.log (`A empresa ${empresa.nome} está localizada em ${empresa.endereço.Rua},
+${empresa.endereço.numero}.Seu segmento de atuação é ${empresa.foco}. `)
+
