be2f735e108f77ad0b582c0868e26baf2194aefb
Vitor Santos <vtrbarranqueiro@hotmail.com>
2020-06-17 21:13:22 -0300

    desafio Objetos, adicionado um array teste

--- desafio-obj.js
+++ desafio-obj.js
@@ -1,13 +1,25 @@
-const empresa = {
-    nome:"Rocketseat",
+const empresa = [
+   { nome:"Rocketseat",
     cor:"Roxo",
     foco:"Programação",
     endereço: {
         Rua: "Rua Guilherme Gembala",
         numero: 260
     }
-}
+   },
 
-console.log (`A empresa ${empresa.nome} está localizada em ${empresa.endereço.Rua},
-${empresa.endereço.numero}.Seu segmento de atuação é ${empresa.foco}. `)
+  { nome:"Tecnique",
+   cor:"azul",
+   foco:"Infraestrutura",
+   endereço: {
+       Rua: "Rua monte carmelo",
+       numero: 126
+   }
+  }
+
+]
+
+
+console.log (`A empresa ${empresa[1].nome} está localizada em ${empresa[1].endereço.Rua},
+${empresa[1].endereço.numero}.Seu segmento de atuação é ${empresa[1].foco}. `)
 
